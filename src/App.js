54739480fffa3000d9b779a7f93e7247fe98e56e
Carlie Duperval <carlierichell@gmail.com>
2022-06-06 23:01:56 -0400

    WORK=TRUE My fronctend work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
 import Reports from "./components/Reports";
 import React, { useEffect, useState } from "react";
+import ReportFilter from "./components/ReportFilter";
 
 function App() {
   const [sales, setSales] = useState([]); // To display the sales
-  const [displaySales, setDisplaySales] = useState([]); // After the filter the sales will keep displaying
+  //const [displaySales, setDisplaySales] = useState([]); // After the filter the sales will keep displaying
+
   useEffect(() => {
     fetch("http://localhost:3030/sales")
       .then((response) => response.json())
       .then((data) => setSales(data))
-      .then((data) => setDisplaySales(data))
+      // .then((data) => setDisplaySales(data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div className="App">
-      <Reports sales={sales} displaySales={displaySales} />
+      <ReportFilter sales={sales} />
+
+      <Reports sales={sales} />
     </div>
   );
 }
--- src/components/ReportFilter.jsx
+++ src/components/ReportFilter.jsx
@@ -1,7 +1,16 @@
 import Autocomplete from "@mui/material/Autocomplete";
+import { TextField } from "@mui/material";
 
-export const ReportFilter = () => {
-  const [reportSearch, setReportSearch] = useState();
-
-  return <Autocomplete options={products} />;
+const ReportFilter = ({ sales }) => {
+  //const [reportSearch, setReportSearch] = useState();
+  return (
+    <Autocomplete
+      options={sales} // this is an array of
+      // filterOptions={(productName) => productName} // ? determine the filtered option to be rendered on search
+      getOptionLabel={(option) => option.productName} // use to determine the disabled state for a given option
+      renderInput={(params) => <TextField {...params} label="Product Name" />}
+    />
+  );
 };
+
+export default ReportFilter;
