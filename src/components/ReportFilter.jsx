f2006e024f7ffca05450cf82833f2ff0f311a59d
Carlie Duperval <carlierichell@gmail.com>
2022-06-07 13:07:09 -0400

    It's OK

--- src/components/ReportFilter.jsx
+++ src/components/ReportFilter.jsx
@@ -1,24 +1,31 @@
 import Autocomplete from "@mui/material/Autocomplete";
-import { TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 
 const ReportFilter = ({ sales }) => {
   //const [reportSearch, setReportSearch] = useState();
   return (
     <>
-      <Autocomplete
-        sx={{ width: 300, height: 70 }}
-        options={sales} // this is an array of
-        getOptionLabel={(option) => option.productName} // use to determine the disabled state for a given option
-        renderInput={(params) => <TextField {...params} label="Product Name" />}
-      />
-      <Autocomplete
-        sx={{ width: 300, height: 70 }}
-        options={sales} // this is an array of
-        getOptionLabel={(productName) => productName.productCategory} // use to determine the disabled state for a given option
-        renderInput={(params) => (
-          <TextField {...params} label="Product Category" />
-        )}
-      />
+      <Grid container spacing={1}>
+        <Autocomplete
+          sx={{ width: 300, height: 70 }}
+          options={sales} // this is an array of
+          getOptionLabel={(option) => option.productName} // use to determine the disabled state for a given option
+          renderInput={(params) => (
+            <TextField {...params} label="Product Name" />
+          )}
+        />
+        {/* <span> */}
+        <Autocomplete
+          id="Category-Autocomplete"
+          sx={{ width: 300, height: 70 }}
+          options={sales} // this is an array of
+          getOptionLabel={(productName) => productName.productCategory} // use to determine the disabled state for a given option
+          renderInput={(params) => (
+            <TextField {...params} label="Product Category" />
+          )}
+        />
+        {/* </span> */}
+      </Grid>
     </>
   );
 };
