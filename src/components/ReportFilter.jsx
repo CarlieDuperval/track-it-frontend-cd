a20477be98780bc25b354d8d59e456e27993d2a7
Carlie Duperval <carlierichell@gmail.com>
2022-06-07 11:11:21 -0400

    Added product category filter

--- src/components/ReportFilter.jsx
+++ src/components/ReportFilter.jsx
@@ -4,11 +4,22 @@ import { TextField } from "@mui/material";
 const ReportFilter = ({ sales }) => {
   //const [reportSearch, setReportSearch] = useState();
   return (
-    <Autocomplete
-      options={sales} // this is an array of
-      getOptionLabel={(option) => option.productName} // use to determine the disabled state for a given option
-      renderInput={(params) => <TextField {...params} label="Product Name" />}
-    />
+    <>
+      <Autocomplete
+        sx={{ width: 300, height: 70 }}
+        options={sales} // this is an array of
+        getOptionLabel={(option) => option.productName} // use to determine the disabled state for a given option
+        renderInput={(params) => <TextField {...params} label="Product Name" />}
+      />
+      <Autocomplete
+        sx={{ width: 300, height: 70 }}
+        options={sales} // this is an array of
+        getOptionLabel={(productName) => productName.productCategory} // use to determine the disabled state for a given option
+        renderInput={(params) => (
+          <TextField {...params} label="Product Category" />
+        )}
+      />
+    </>
   );
 };
 
