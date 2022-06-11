f8d1f25d5fc1a0cfec132d66d30e0514793b2118
Carlie Duperval <carlierichell@gmail.com>
2022-06-11 09:23:23 -0400

    Added Year autocomplite, Getting a waring to fix Year as a string

--- src/components/ReportFilter.jsx
+++ src/components/ReportFilter.jsx
@@ -1,28 +1,40 @@
 import Autocomplete from "@mui/material/Autocomplete";
 import { Grid, TextField } from "@mui/material";
-//import { useState } from "react";
+import { gridDensityHeaderHeightSelector } from "@mui/x-data-grid";
+import { handleBreakpoints } from "@mui/system";
 
 const ReportFilter = ({ sales }) => {
-  //const [report, setReport] = useState([]);
   const handleSubmit = (e) => {
     console.log("here ->", e);
   };
 
+  const getProductName = () => {
+    const prodName = sales.map((s) => s.productName);
+    return [...new Set(prodName)];
+  };
   const getCategories = () => {
     const cats = sales.map((s) => s.productCategory);
 
     return [...new Set(cats)]; // set each category
+  };
+  const getYearSalesQty = () => {
+    const year = sales.map((s) => s.year);
+    return [...new Set(year)];
   };
   return (
     <>
       <Grid container spacing={1}>
         <Autocomplete
           sx={{ width: 300, height: 70 }}
-          options={sales} // an array
-          getOptionLabel={(option) => option.productName} // determine a string value for a given option
-          renderInput={(params) => (
-            <TextField {...params} label="Product Name" />
-          )}
+          options={getProductName()}
+          //options={sales} // an array
+          //getOptionLabel={(option) => option.productName} // determine a string value for a given option
+          onSelect={(e) => handleSubmit(e.target.value)}
+          renderInput={(params) => {
+            return (
+              <TextField {...params} key={params.id} label="Product Name" />
+            );
+          }}
         />
 
         <Autocomplete
@@ -45,19 +57,21 @@ const ReportFilter = ({ sales }) => {
             );
           }}
         />
-
-        {/* <FormControl>
-          <Input
-            sx={{ width: 300, height: 70 }}
-            type="text"
-            value={report} // calling sales from myBackend
-            onChange={(e) => setReport(e.target.value)}
-            renderInput={(params) => (
-              <TextField {...params} label="Report By Category" />
-            )}
-          />
-          <Button onClick={() => handleSubmit()}>Submit</Button>
-        </FormControl> */}
+        <Autocomplete
+          sx={{ width: 300, height: 70 }}
+          options={getYearSalesQty()}
+          onSelect={(e) => handleBreakpoints(e.target.value)}
+          renderInput={(params) => {
+            return <TextField {...params} key={params.id} label="Year" />;
+          }}
+          renderOption={(props, sale) => {
+            return (
+              <li {...props} key={sale}>
+                {sale}
+              </li>
+            );
+          }}
+        />
       </Grid>
       <></>
     </>
