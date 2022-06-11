c960b639fae6302b6394721ec8d9fc35f34d9317
Carlie Duperval <carlierichell@gmail.com>
2022-06-11 08:24:32 -0400

    commited  Home TexHeader

--- src/components/Home/Heros.jsx
+++ src/components/Home/Heros.jsx
@@ -6,11 +6,13 @@ const Hero = () => {
   return (
     <div>
       <div className="textBlock">
-        <div className="allText">
-          TrackIT is keep tracking sales for better decision Making.
-        </div>
+        <div className="allText">Welcome To TrackIT</div>
         <div className="textCell">
-          <p className="textHeader">Sales need to be track, Let's TrackIT</p>
+          <p className="textHeader">
+            Sales need to be track for better decision Making. <br />
+            This App allows decision makers query quick report their monthly
+            sales. <br /> Let's TrackIT
+          </p>
           <Button className="button" onClick={handleLogin}>
             Log In
           </Button>
